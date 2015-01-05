a1101c15d14e0c56777300d5f948211d796b831b
ArchAiA <xarchaiax@gmail.com>
2015-01-04 21:31:03 -0500

    Finished Lecture 47

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+	title = models.CharField(max_length=120, null=False, blank=False)
+	description = models.TextField(null=True, blank=True)
+	price = models.DecimalField(decimal_places=2, max_digits=100, default=29.99)
+	slug = models.SlugField() # A Slug Field will create urls from the title
+	active = models.BooleanField(default=True)
+
+	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False) # First added to database
+	update = models.DateTimeField(auto_now_add=False, auto_now=True) # Last time the instance was changed
+	shipping_date = models.DateTimeField(auto_now_add=False, auto_now=False, null=True, blank=True) # Unless null, and blank are both true, the attribute will be requried
+
+	def __unicode__(self): # Every time we save the instance it will return the title as unicode
+		return self.title
+
+	def get_price(self):
+		return self.price
+
+
--- src/products/views.py
+++ src/products/views.py
@@ -9,6 +9,6 @@ def home(request):
 	else:
 		context = {'username_is': request.user}
 
-	template = 'base.html'
+	template = 'products/home.html'
 	
 	return render(request, template, context)
